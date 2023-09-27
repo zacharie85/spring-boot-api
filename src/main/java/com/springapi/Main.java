ebc6dedbef4ad8242923e2a8d5206c250ea428aa
Zacharie kouemegni <zachariekouemegni@zacharies-mbp.home>
2023-09-27 11:29:29 -0400

    save customer

--- src/main/java/com/springapi/Main.java
+++ src/main/java/com/springapi/Main.java
@@ -2,17 +2,41 @@ package com.springapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main (CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
+    }
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
     }
-    @GetMapping("/")
-    public String great(){
-        return "HelLO";
+
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
+    }
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ){
+
+    }
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request){
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
     }
 }
