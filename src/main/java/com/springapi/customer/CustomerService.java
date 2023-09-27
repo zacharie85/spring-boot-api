3f6d805726f73df48896943fcc8077c3ab76135c
Zacharie kouemegni <zachariekouemegni@zacharies-mbp.home>
2023-09-27 13:42:28 -0400

    structure code

--- src/main/java/com/springapi/customer/CustomerService.java
+++ src/main/java/com/springapi/customer/CustomerService.java
@@ -1,9 +1,12 @@
 package com.springapi.customer;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
@@ -15,31 +18,46 @@ public class CustomerService {
         this.customerRepository = customerRepository;
     }
 
-    public List<Customer> getCustomers(){
+    public List<Customer> getCustomers() {
         return customerRepository.findAll();
     }
-    public void addCustomer(Customer customer){
+
+    public void addCustomer(Customer customer) {
         customerRepository.save(customer);
     }
 
-    public void deleteCustomer(Integer id){
+    public void deleteCustomer(Integer id) {
         boolean exist = customerRepository.existsById(id);
-        if(!exist){
-            throw  new IllegalStateException(
+        if (!exist) {
+            throw new IllegalStateException(
                     "Customer with id " + id + " does not exist"
             );
         }
         customerRepository.deleteById(id);
     }
 
+    @Transactional
     public void updateCustomer(Integer id,
-                               Customer customer){
-        boolean exist = customerRepository.existsById(id);
-        if(!exist){
-            throw  new IllegalStateException(
-                    "Customer with id " + id + " does not exist"
-            );
+                               Customer customer) {
+        Customer customer1 = customerRepository.findById(id)
+                .orElseThrow(() -> {
+                    return new IllegalStateException(
+                            "Customer with id " + id + " does not exist"
+                    );
+                });
+
+        if (!Objects.equals(customer.getName(),customer1.getName())){
+            customer1.setName(customer.getName());
         }
-        customerRepository.save(customer);
-    }
+
+        if (!Objects.equals(customer.getAge(),customer1.getAge())){
+            customer1.setAge(customer.getAge());
+        }
+
+        if (!Objects.equals(customer.getEmail(),customer1.getEmail())){
+            customer1.setEmail(customer.getEmail());
+        }
+    };
 }
+
+
